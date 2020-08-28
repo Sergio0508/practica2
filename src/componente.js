99c918b59e3ea4f917f15b75ea8ea563cfb53bda
Dayana998 <dayis12456@gmail.com>
2020-08-27 23:47:55 -0400

    subiendo los archivos de la practica 2

---
+++ src/componente.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './hola.css'
+
+class Hola extends Component {
+
+    render() {
+        return (
+            <div>
+            <h1> HOLA MUNDO DESDE REACT!! </h1>
+            <h2>
+            Este es mi primer "Hola Mundo" desde ReactJS
+            </h2>
+            </div>
+        );
+    }
+}
+export default Hola;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Hola from './componente'
+
+render( <Hola />, document.getElementById('root'))
